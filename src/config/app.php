619faba2181182d43ecb1af8b7e2ea4c4b0a5838
hirosuke-pi <betacode.net@gmail.com>
2023-07-03 14:06:54 +0900

    add tag provider追加

--- src/config/app.php
+++ src/config/app.php
@@ -170,6 +170,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\UserUseCaseProvider::class,
         App\Providers\NewsUseCaseProvider::class,
+        App\Providers\TagUseCaseProvider::class,
     ])->toArray(),
 
     /*
