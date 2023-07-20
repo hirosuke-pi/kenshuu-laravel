d5af2d0f4e0b547113d04fa93d44ac12d3a9d0ca
hirosuke-pi <betacode.net@gmail.com>
2023-07-20 17:43:39 +0900

    fix メソッドで分割するように修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -43,6 +43,6 @@ Route::group(['middleware' => ['login.user']], function() {
     });
 });
 
-Route::post('/auth/login', [AuthController::class, 'login'])->name('auth.login');
-Route::get('/auth/logout', [AuthController::class, 'logout'])->name('auth.logout');
-Route::post('/auth/register', [AuthController::class, 'register'])->name('auth.register');
+Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
