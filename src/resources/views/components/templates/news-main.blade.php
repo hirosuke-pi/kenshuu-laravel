d5781380905a9b36b172240f6921133abe995009
hirosuke-pi <betacode.net@gmail.com>
2023-07-24 11:19:35 +0900

    fix 条件分岐修正

--- src/resources/views/components/templates/news-main.blade.php
+++ src/resources/views/components/templates/news-main.blade.php
@@ -1,6 +1,6 @@
 <div class="w-full lg:w-3/6 ">
     <x-organisms.breadcrumb-section :paths="$paths" />
-    @if (true)
+    @if ($isAdmin && !$isEditorMode)
         <x-molecules.news-action :news="$news" />
     @endif
     @if ($isEditorMode)
